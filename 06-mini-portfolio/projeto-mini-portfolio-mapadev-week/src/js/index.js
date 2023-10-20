cf7dcf57b0ceaf8912bf334d55c4e00f52310a0f
Danilo Cavalcante <dndancv@gmail.com>
2023-10-19 22:04:15 -0300

    Comentários removidos

--- 06-mini-portfolio/projeto-mini-portfolio-mapadev-week/src/js/index.js
+++ 06-mini-portfolio/projeto-mini-portfolio-mapadev-week/src/js/index.js
@@ -1,8 +1,6 @@
-// passo 1 - dar um jeito de pegar os elementos que representam as abas no HTML
 const abas = document.querySelectorAll(".aba");
 
 abas.forEach((aba) => {
-  // passo 2 - dar um jeito de identificar o clique no elemento da aba
   aba.addEventListener("click", function () {
     if (aba.classList.contains("selecionado")) return;
 
@@ -12,11 +10,9 @@ abas.forEach((aba) => {
 });
 
 function selecionarAba(aba) {
-  // passo 3 - desmarcar a aba selecionada
   const abaSelecionada = document.querySelector(".aba.selecionado");
   abaSelecionada.classList.remove("selecionado");
 
-  // passo 4 - marcar a aba clicada como selecionado
   aba.classList.add("selecionado");
 }
 
@@ -24,10 +20,8 @@ function mostrarInformacoesDaAba(aba) {
   const idDoElementoDeInformacoesDaAba = `informacao-${aba.id}`;
   const informacaoASerMostrada = document.getElementById(idDoElementoDeInformacoesDaAba);
 
-  // passo 5 - esconder o conteúdo anterior
   const informacaoSelecionada = document.querySelector(".informacao.selecionado");
   informacaoSelecionada.classList.remove("selecionado");
 
-  // passo 6 - mostrar o conteúdo da aba selecionada
   informacaoASerMostrada.classList.add("selecionado");
 }
